79a5d5f6d96839a1e3cc339cbd47e5a7117abdad
Rizqi Arief Wicaksono <rizqiariefwicaksono@MBP-Rizqi.mshome.net>
2025-10-08 01:06:36 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pertemuan1
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func SapaHello() {
+	fmt.Println("Hallo World")
+}
+
+func main() {
+	fmt.Println("Ini Jalan di Go")
+	SapaHello()
+}
